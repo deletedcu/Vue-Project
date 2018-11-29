9b6e48206ed771b555fbefea3aff69106a2f4c17
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2018-11-30 04:05:33 +0800

    updated the package.json

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import { sync } from 'vuex-router-sync';
+import Vuetify from 'vuetify';
+import URLSearchParams from 'url-search-params';
 import App from './App'
 import router from './router'
 
