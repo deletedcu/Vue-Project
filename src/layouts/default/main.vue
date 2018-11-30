c7d0024a490d6a5c0bd2c87b1856c0cbb0c53124
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2018-12-01 00:34:27 +0800

    Update layouts

--- src/layouts/default/main.vue
+++ src/layouts/default/main.vue
@@ -1,20 +1,26 @@
 <template lang="pug">
   v-app.my-default-layout
-    //- app-sidebar
-    //- app-bar
+    app-sidebar
+    app-bar
     v-content(style="padding-top: 48px;")
       transition(name="slide" mode="out-in")
         router-view
-    //- app-footer
-    //- app-dialog
-    //- app-snackbar
+    app-footer
+    app-dialog
+    app-snackbar
 </template>
 
 <script>
+import AppBar from '@/components/app-bar'
+import AppSidebar from '@/components/app-sidebar'
+import AppDialog from '@/components/app-dialog'
+import AppSnackbar from '@/components/app-snackbar'
+import AppFooter from '@/components/app-footer'
+
 export default {
   name: 'DefaultLayout',
 
-  components: {  },
+  components: { AppBar, AppSidebar, AppDialog, AppSnackbar, AppFooter },
 
   methods: {
 
@@ -23,5 +29,4 @@ export default {
 </script>
 
 <style lang="stylus">
-
 </style>
