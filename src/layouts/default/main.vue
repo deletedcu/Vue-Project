4ced7a92b3047973bb6055ac48c4a7d3902eea62
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2018-11-30 11:05:49 +0800

    Define the layouts

---
+++ src/layouts/default/main.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  v-app.my-default-layout
+    //- app-sidebar
+    //- app-bar
+    v-content(style="padding-top: 48px;")
+      transition(name="slide" mode="out-in")
+        router-view
+    //- app-footer
+    //- app-dialog
+    //- app-snackbar
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+
+  components: {  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
