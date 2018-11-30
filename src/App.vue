86df9447d96a9abd2e1b819039ab7ac4ab1b34ad
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2018-12-01 00:31:43 +0800

    Update main files

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    component(:is="component")
+      slot
 </template>
 
 <script>
+import PublicLayout from '@/layouts/public/main.vue'
+import DefaultLayout from '@/layouts/default/main.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+
+  components: {
+    PublicLayout,
+    DefaultLayout
+  },
+
+  computed: {
+    component () {
+      return this.$store.state.common.layout
+    }
+  },
+
+  mounted () {
+    // Update page title.
+    this.$store.watch((state) => {
+      return state.common.title
+    }, (title) => {
+      document.title = title
+      console.log('title updated')
+    }, {
+      deep: true
+    })
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,49 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import { sync } from 'vuex-router-sync';
-import Vuetify from 'vuetify';
-import URLSearchParams from 'url-search-params';
-import App from './App'
-import router from './router'
+import store from './store'
+import { sync } from 'vuex-router-sync'
+import { http, router } from './http'
+import auth from './auth'
+import Vuetify from 'vuetify'
+import URLSearchParams from 'url-search-params'
+import App from './app'
+import Loading from './components/loading'
+import Appbar from './components/app-bar'
+import Appfooter from './components/app-footer'
 
 Vue.config.productionTip = false
+
+// Polyfills
+global.URLSearchParams = URLSearchParams
+
+// Sync router to store, as `store.state.route`.
+sync(store, router)
+
+// Http and Auth plugins
+Vue.use(http)
+Vue.use(auth)
+
+// Vuetify
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#21CE99',
+    secondary: '#D81B60',
+    accent: '#805441'
+  }
+})
+
+// Styles
+require('./styles/scss/main.scss')
+require('./styles/stylus/main.styl')
+
+// Global Components
+Vue.component('loading', Loading)
+Vue.component('Appbar', Appbar)
+Vue.component('Appfooter', Appfooter)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  render: h => h(App)
 })
